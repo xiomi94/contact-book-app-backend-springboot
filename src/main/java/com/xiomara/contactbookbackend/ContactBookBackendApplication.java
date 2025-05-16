44b2d8ef0ff0efa895a606851b92d9da5f1d4d54
xiomi94 <xiomarajimvel94@gmail.com>
2025-05-16 11:57:00 +0100

    Init project

---
+++ src/main/java/com/xiomara/contactbookbackend/ContactBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xiomara.contactbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ContactBookBackendApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/xiomara/contactbookbackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.xiomara.contactbookbackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ContactBookBackendApplication.class);
+  }
+
+}
